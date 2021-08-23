72800ca5c7a35dfae52401559ea3d975a2d29d6d
vk0808 <vk4765712@gmail.com>
2021-08-23 13:13:30 +0530

    feat: welcome msg

---
+++ MoodAnalyser1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyser1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyser");
+        }
+    }
+}
